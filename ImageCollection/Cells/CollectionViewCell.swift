1a61b931aece0c12a089f5634775de1fc8e33010
Meet's Mac <Meetbudheliya04@gmail.com>
2022-08-30 12:13:57 +0530

    Initial commit

---
+++ ImageCollection/Cells/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  ImageCollection
+//
+//  Created by Meet's MAC on 30/08/22.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var img_image: UIImageView!
+    
+}
